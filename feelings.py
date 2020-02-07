a8e332697fefd6f16c2dd944a5a2eee9384cb959
nischint <60770088+nischint@users.noreply.github.com>
2020-02-07 13:53:00 +0530

    Create feelings.py

---
+++ feelings.py
@@ -0,0 +1,19 @@
+
+import tweepy as tp
+import time
+# credentials to login to twitter ap
+consumer_key = '4RYCTuQcj3T4QvrgBqcAOMXKn'
+consumer_secret = 'PPVmrZq8Wjbwh5lHKxclTpmNReMESipEHYaxL61ApKYsiNstrf'
+access_token = '1225651180115677184-piBnM2AyURQNlFVOIo7tw3HGE0scCu'
+access_secret = 'Vsc5zXWVbnta1wv1bnFDwR8qn8ihZvM8Avb1ZsaAtTB9h'
+
+# login to twitter account api
+auth = tp.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tp.API(auth)
+
+
+
+# update status
+for api.update_status("i love you, ❤️")
+    time.sleep(3)
